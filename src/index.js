3d97d8ea1b4bf01528c99a1101bc996d906c5671
Joe Cserko <j.cserko@gmail.com>
2021-05-12 12:34:32 +0200

    lesson: api-03

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use('/person', require('./controllers/person/routes'));
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
