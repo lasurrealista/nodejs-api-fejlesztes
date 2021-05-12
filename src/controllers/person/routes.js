fe31fc5d34ca77482fc0bc6022687f067ebe0723
Joe Cserko <j.cserko@gmail.com>
2021-05-12 12:51:35 +0200

    lesson: api-04

--- src/controllers/person/routes.js
+++ src/controllers/person/routes.js
@@ -7,4 +7,30 @@ controller.get('/', (req, res) => {
     res.json(data);
 });
 
+// Create a new person.
+controller.post('/', (req, res) => {
+    const newPerson = req.body;
+    newPerson.id = data[data.length - 1].id + 1;
+    data.push(newPerson);
+
+    res.status(201);
+    res.json(newPerson);
+});
+
+// Update a person.
+controller.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const index = data.findIndex(p => p.id === Number(id));
+    const {first_name, last_name, email} = req.body;
+
+    data[index] = {
+        id,
+        first_name,
+        last_name,
+        email
+    };
+
+    res.json(data[index]);
+});
+
 module.exports = controller;
